06f5cfbcc646e189f1bdfcac84ceab4254c3e375
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-10-22 08:32:38 +0000

    Add queryID to query snapshot

--- lib/src/query_snapshot.dart
+++ lib/src/query_snapshot.dart
@@ -13,11 +13,12 @@ class AlgoliaQuerySnapshot {
             .toList()
             .cast<AlgoliaObjectSnapshot>(),
         empty = (map['hits'] as List<dynamic>? ?? []).isEmpty,
+        length = map['length'] ?? 0,
         nbHits = map['nbHits'],
-        page = map['page'] ?? 0,
         nbPages = map['nbPages'] ?? 0,
         offset = map['offset'] ?? 0,
-        length = map['length'] ?? 0,
+        page = map['page'] ?? 0,
+        queryId = map['queryID'],
         hitsPerPage = map['hitsPerPage'] ?? 0,
         processingTimeMS = map['processingTimeMS'],
         exhaustiveNbHits = map['exhaustiveNbHits'],
@@ -29,11 +30,12 @@ class AlgoliaQuerySnapshot {
   final List<AlgoliaObjectSnapshot> hits;
   final Algolia algolia;
   final bool empty;
+  final int length;
   final int nbHits;
-  final int page;
   final int nbPages;
   final int offset;
-  final int length;
+  final int page;
+  final String queryId;
   final int hitsPerPage;
   final int processingTimeMS;
   final bool exhaustiveNbHits;
